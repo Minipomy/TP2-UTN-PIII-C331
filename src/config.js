c912de30a555de960501280fb5ea95c1c5d82841
minipomy <minipomy14@gmail.com>
2024-11-25 16:25:54 -0300

    Added safety condition if .env file is not present - Proceed

--- src/config.js
+++ src/config.js
@@ -1,16 +1,21 @@
 const dotenv = require('dotenv')
 const path = require('path')
+const fs = require('fs')
 
-dotenv.config({
-    path: path.resolve('../', '.env')
-})
+const envPath = path.resolve('../', '.env');
+
+if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath });
+} else {
+    console.warn('.env file not found. Loading default configurations.');
+}
 
 module.exports = {
-    DIALECT: process.env.DB_DIALECT,
-    DB_NAME: process.env.DB_NAME,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    S_PORT: process.env.S_PORT
+    DIALECT: process.env.DB_DIALECT || 'mysql', 
+    DB_NAME: process.env.DB_NAME || 'hardware_store',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || '',
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_PORT: process.env.DB_PORT || '3306',
+    S_PORT: process.env.S_PORT || '3000'
   }
